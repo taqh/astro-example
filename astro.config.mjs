feedc0d0271964b441326692597ee6f1277eb3d7
Taqib <taqib.ibrahim@gmail.com>
2025-01-19 02:13:23 +0100

    added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,21 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://coconutsinwinter.vercel.app',
-  experimental: {
-    svg: true,
-  },
+
   // image: {
   //   remotePatterns: [{ protocol: 'https' }],
   // },
+  experimental: {
+    svg: true,
+  },
+
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
