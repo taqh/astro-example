56ac38f7cf61957130f2a6604c15241280ab4ae1
Taqib <taqib.ibrahim@gmail.com>
2025-01-19 01:53:15 +0100

    fix type in og image link, added rss link to head tag

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,5 +1,8 @@
 export const SITE = {
-  TITLE: 'Coconuts in wnter',
+  TITLE: 'Coconuts in winter',
   DESCRIPTION: 'Chilling ghost tales from the west african tropics',
   URL: 'https://coconutsinwinter.vercel.app',
+  OPEN_GRAPH: {
+    IMAGE: 'https://coconutsinwinter.vercel.app/og.png',
+  }
 };
